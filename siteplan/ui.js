697ffaf3f2792306da2fe2eb1a4f9aab774cc123
junglistkean <junglistkean@googlemail.com>
2026-04-11 13:42:05 +0100

    Update ui.js

--- siteplan/ui.js
+++ siteplan/ui.js
@@ -1404,7 +1404,7 @@ function exportPlan() {
   const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
   const a = document.createElement('a');
   a.href = URL.createObjectURL(blob);
-  a.download = (State.planTitle || 'siteplan') + '.json';
+  a.download = (State.planTitle || 'siteplan').toLowerCase().replace(/\s+/g, '-') + '.json';
   a.click();
 }
 
